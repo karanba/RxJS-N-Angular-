41f7ca90f4010382b63106578839e9ba9496f430
karanba <altaykarakus@gmail.com>
2022-07-21 01:03:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-rty2dm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-rty2dm)
---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, VERSION } from '@angular/core';
+import { of, from, map } from 'rxjs';
+import { take, tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  name = 'Angular ' + VERSION.major;
+
+  ngOnInit(): void {
+    of(2, 4, 6, 8).subscribe((item) => console.log(item));
+
+    from([20, 15, 10, 5]).subscribe({
+      next: (item) => console.log(`resulting item .. ${item}`),
+      error: (err) => console.log(`error occured .. ${err}`),
+      complete: () => console.log(`complete`),
+    });
+
+    of('app1', 'app2', 'app3').subscribe({
+      next: (item) => console.log(`resulting item .. ${item}`),
+      error: (err) => console.log(`error occured .. ${err}`),
+      complete: () => console.log(`complete`),
+    });
+
+    of(2, 4, 6)
+      .pipe(
+        map((x) => x * 3),
+        tap((x) => console.log('tab', x)),
+        take(3)
+      )
+      .subscribe((x) => console.log(x));
+  }
+}
